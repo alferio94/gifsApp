8a65690c7f30a18c96da984e9d2b56e241a904e1
Alfonso Carmona <alfonso.carmona.94@outlook.com>
2021-06-02 14:21:15 -0500

    Added gifs module and components also sidebar comp

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,16 @@
+import { ElementRef } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent {
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+  buscar(){
+    const valor = this.txtBuscar.nativeElement.value;
+    this.txtBuscar.nativeElement.value = '';
+  }
+}
